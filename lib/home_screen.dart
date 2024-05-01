93afffaddeb998d7077b7adb0bd04cb9a0342627
fokrul511 <islamfokrul2002@gmail.com>
2024-05-01 12:19:11 +0600

    Orintation Update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:photo_gallery_assignment/show_image.dart';
-
 import 'image_list.dart';
-
-
 
 class PhotoGallery extends StatefulWidget {
   const PhotoGallery({super.key});
@@ -16,79 +13,151 @@ class _PhotoGalleryState extends State<PhotoGallery> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Photo Gallery"),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.more_vert,
-            ),
-          ),
-        ],
-      ),
-      body: GridView.builder(
-        itemCount: photoGalleryList.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 2,
-          mainAxisSpacing: 2,
-        ),
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ShowImageScreen(
-                        title: photoGalleryList[index]['title'] ?? '',
-                        image: photoGalleryList[index]['image'] ?? '',
-                        ditails: photoGalleryList[index]['Ditails'] ?? ''),
-                  ),
-                );
-              },
-              child: Stack(
-                children: [
-                  Card(
-                    elevation: 10,
-                    shape: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
-                    ),
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 200,
-                      // Adjust the height according to your requirement
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(30),
-                        child: Image.asset(
-                          photoGalleryList[index]['image'].toString(),
-                          // Replace with your image URL
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 16,
-                    left: 16,
-                    child: Text(
-                      photoGalleryList[index]['title'].toString(),
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
+        appBar: AppBar(
+          title: const Text("Photo Gallery"),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert,
               ),
             ),
-          );
-        },
-      ),
-    );
+          ],
+        ),
+        body: OrientationBuilder(
+          builder: (context, orientation) => orientation == Orientation.portrait
+              ? buildportat()
+              : buillandScope(),
+        ));
   }
+}
+
+Widget buildportat() {
+  return GridView.builder(
+    itemCount: photoGalleryList.length,
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2,
+      crossAxisSpacing: 2,
+      mainAxisSpacing: 2,
+    ),
+    itemBuilder: (context, index) {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ShowImageScreen(
+                    title: photoGalleryList[index]['title'] ?? '',
+                    image: photoGalleryList[index]['image'] ?? '',
+                    ditails: photoGalleryList[index]['Ditails'] ?? ''),
+              ),
+            );
+          },
+          child: Stack(
+            children: [
+              Card(
+                elevation: 10,
+                shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 200,
+                  // Adjust the height according to your requirement
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: Image.asset(
+                      photoGalleryList[index]['image'].toString(),
+                      // Replace with your image URL
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 16,
+                left: 16,
+                child: Text(
+                  photoGalleryList[index]['title'].toString(),
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
+
+Widget buillandScope() {
+  return GridView.builder(
+    itemCount: photoGalleryList.length,
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 4,
+      crossAxisSpacing: 2,
+      mainAxisSpacing: 2,
+    ),
+    itemBuilder: (context, index) {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ShowImageScreen(
+                    title: photoGalleryList[index]['title'] ?? '',
+                    image: photoGalleryList[index]['image'] ?? '',
+                    ditails: photoGalleryList[index]['Ditails'] ?? ''),
+              ),
+            );
+          },
+          child: Stack(
+            children: [
+              Card(
+                elevation: 10,
+                shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 200,
+                  // Adjust the height according to your requirement
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: Image.asset(
+                      photoGalleryList[index]['image'].toString(),
+                      // Replace with your image URL
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 16,
+                left: 16,
+                child: Text(
+                  photoGalleryList[index]['title'].toString(),
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
 }
--- lib/show_image.dart
+++ lib/show_image.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'image_list.dart';
 
@@ -20,12 +21,6 @@ class ShowImageScreen extends StatelessWidget {
         leading: IconButton(
             onPressed: () {
               Navigator.pop(context);
-              // Navigator.pushReplacement(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) => const PhotoGallery(),
-              //   ),
-              // );
             },
             icon: const Icon(Icons.arrow_back_ios_new_sharp)),
         actions: [
@@ -36,36 +31,181 @@ class ShowImageScreen extends StatelessWidget {
             ),
           ),
         ],
-        title: Text(title ),
+        title: Text(title),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.all(15.0),
+          child: OrientationBuilder(
+            builder: (context, orientation) =>
+                orientation == Orientation.portrait
+                    ? buildPortait(image, title, ditails)
+                    : buildlandScope(context, image, title, ditails),
+          )),
+    );
+  }
+}
+
+Widget buildPortait(String image, String title, String ditails) {
+  return Column(
+    children: [
+      Expanded(
+        flex: 3,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              flex: 3,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            Container(
+              height: 250,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(image), fit: BoxFit.cover),
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, left: 20),
+              child: Text(
+                title,
+                style: const TextStyle(fontSize: 24),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, bottom: 10, right: 10),
+              child: Text(
+                ditails,
+                style: const TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 51,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "See More",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 10, left: 20),
+              child: Text(
+                "Suggestions",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0xff2CAB00),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      Expanded(
+          child: GridView.builder(
+        itemCount: photoGalleryList.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 5,
+        ),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ShowImageScreen(
+                        title: photoGalleryList[index]['title'] ?? '',
+                        image: photoGalleryList[index]['image'] ?? '',
+                        ditails: photoGalleryList[index]['Ditails'] ?? ''),
+                  ),
+                );
+              },
+              child: Stack(
                 children: [
-                  Container(
-                    height: 250,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(image), fit: BoxFit.cover),
+                  Card(
+                    elevation: 10,
+                    shape: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 200,
+                      // Adjust the height according to your requirement
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Image.asset(
+                          photoGalleryList[index]['image'].toString(),
+                          // Replace with your image URL
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10, left: 20),
+                  Positioned(
+                    bottom: 16,
+                    left: 16,
                     child: Text(
-                      title,
-                      style: const TextStyle(fontSize: 24),
+                      photoGalleryList[index]['title'].toString(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(left: 20, bottom: 10, right: 10),
+                ],
+              ),
+            ),
+          );
+        },
+      ))
+    ],
+  );
+}
+
+buildlandScope(
+    BuildContext context, String image, String title, String ditails) {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Container(
+        height: MediaQuery.of(context).size.height * 10,
+        width: MediaQuery.of(context).size.width * 0.5,
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 4,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    title,
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 20, bottom: 10, right: 10),
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                    width: MediaQuery.of(context).size.width * 0.4,
                     child: Text(
                       ditails,
                       style: const TextStyle(
@@ -73,11 +213,11 @@ class ShowImageScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, top: 10),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.4,
                     height: 51,
                     child: ElevatedButton(
                       onPressed: () {},
@@ -87,86 +227,22 @@ class ShowImageScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 10, left: 20),
-                    child: Text(
-                      "Suggestions",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Color(0xff2CAB00),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Expanded(
-                child: GridView.builder(
-              itemCount: photoGalleryList.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 5,
-              ),
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ShowImageScreen(
-                              title: photoGalleryList[index]['title'] ?? '',
-                              image: photoGalleryList[index]['image'] ?? '',
-                              ditails:
-                                  photoGalleryList[index]['Ditails'] ?? ''),
-                        ),
-                      );
-                    },
-                    child: Stack(
-                      children: [
-                        Card(
-                          elevation: 10,
-                          shape: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide.none,
-                          ),
-                          child: SizedBox(
-                            width: double.infinity,
-                            height: 200,
-                            // Adjust the height according to your requirement
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(30),
-                              child: Image.asset(
-                                photoGalleryList[index]['image'].toString(),
-                                // Replace with your image URL
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 16,
-                          left: 16,
-                          child: Text(
-                            photoGalleryList[index]['title'].toString(),
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 10, left: 20),
+                  child: Text(
+                    "Suggestions",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color(0xff2CAB00),
                     ),
                   ),
-                );
-              },
-            ))
-          ],
-        ),
-      ),
-    );
-  }
+                )
+              ],
+            ),
+          ),
+        ],
+      )
+    ],
+  );
 }
